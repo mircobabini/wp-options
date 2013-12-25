ba12517ac7f481cf00ae173f64be6888d2677fbe
Mirco Babini <mirkolofio@gmail.com>
2013-12-25 17:06:08 +0100

    removed some crumbs, ghosts support

--- readme.txt
+++ readme.txt
@@ -4,14 +4,14 @@ Donate link: http://mircobabini.com/donate/
 Tags: opt, option, options, manager, editor, theme, plugin
 Requires at least: 2.8
 Tested up to: 3.8
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Easily manage Wordpress Options by the Settings > Options (options.php) page
+Easily manage Wordpress Options by Settings > Options
 
 == Description ==
-Adds an 'Options' link in Settings, linking to the options.php page.
+Adds a Settings > Options link to the options.php page.
 If you wanna contribute, pull requests on [github.com](https://github.com/mirkolofio/wp-options).
 
 Developed by [Mirco Babini](http://www.mircobabini.com/), **Wordpress Consultant, Web Developer and Data Lover**.
@@ -26,6 +26,10 @@ Developed by [Mirco Babini](http://www.mircobabini.com/), **Wordpress Consultant
 == Screenshots ==
 
 == Changelog ==
+
+= 1.0.2 =
+* Removed some crumbs
+* Ghosts support
 
 = 1.0.1 =
 * First release, it just works.
--- wp-options.php
+++ wp-options.php
@@ -2,24 +2,19 @@
 /*
 Plugin Name: Options
 Plugin URI: http://github.com/mirkolofio/wp-options-dashboard
-Description: Easily manage Wordpress Options by the Settings > Options (options.php) page
+Description: Easily manage Wordpress Options. Once activated you'll find me under <strong>Settings &rarr; Options</strong>.
 Author: Mirco Babini
-Version: 1.0.1
+Version: 1.0.2
 Author URI: http://github.com/mirkolofio
 */
 if (is_admin ()) {
 	add_action ('admin_menu' , function () {
 		global $submenu;
 		$submenu['options-general.php'][666] = array( 'Options', 'manage_options' , '/wp-admin/options.php' ); 
-		$submenu['plugins.php'][4] = array( '', 'manage_options' , '/wp-admin/plugins.php?plugin_status=active' ); 
-		ksort ($submenu['plugins.php']);
 	});
 
-	if (!isset ($_GET['ghosts'])) {
-		
-		add_filter ('all_plugins', function ($plugins) {
-			unset ($plugins[plugin_basename (__FILE__)]);
-			return $plugins;
-		});
+	// read more about the ghosts support: http://wordpress.org/plugins/ghosts/
+	if (defined('WP_ENABLE_GHOSTS')) {
+		WPGhosts::ghostize (__FILE__);
 	}
 }
